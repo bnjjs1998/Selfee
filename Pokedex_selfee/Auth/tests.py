4fd9c1f735f8e518ec9862e50bba7bc576e02f3d
bnjjs1998 <b_jaques@hetic.eu>
2026-07-03 16:39:02 +0200

    ecriture des test

--- Pokedex_selfee/Auth/tests.py
+++ Pokedex_selfee/Auth/tests.py
@@ -3,7 +3,8 @@ from django.urls import reverse
 
 
 class HelloWorldViewTests(TestCase):
+
     def test_hello_world_route_returns_expected_response(self):
         response = self.client.get(reverse('hello_world'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Hellogg World !')
+        self.assertContains(response, 'Hello World !')
