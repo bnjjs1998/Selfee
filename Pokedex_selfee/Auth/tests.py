6306d5dced4837a0087d344a4ca540159d5ddbc9
bnjjs1998 <b_jaques@hetic.eu>
2026-07-03 16:23:00 +0200

    premier test de l'application écris

---
+++ Pokedex_selfee/Auth/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+class HelloWorldViewTests(TestCase):
+    def test_hello_world_route_returns_expected_response(self):
+        response = self.client.get(reverse('hello_world'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Hello World !')
