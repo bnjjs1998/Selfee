eada7781c2817f4a2802afbbf3a9295b14f2dc48
bnjjs1998 <b_jaques@hetic.eu>
2026-07-03 16:35:28 +0200

    ajout du login il manque les tests de la vue login

--- Pokedex_selfee/Auth/urls.py
+++ Pokedex_selfee/Auth/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from django.http import HttpResponse
 
+from Pokedex_selfee.Auth.views import LoginView
+
 # On définit une vue simple directement ici
 def hello_world(request):
     return HttpResponse("Hello World !")
 
 urlpatterns = [
     path('hello/', hello_world, name='hello_world'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
