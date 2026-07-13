1b6004ca0674145df36410094fae0fd14cb30c0e
bnjjs1998 <b_jaques@hetic.eu>
2026-07-13 15:47:56 +0200

    mise à jours de la route pokemon

--- Pokedex_selfee/Pokedex_App/urls.py
+++ Pokedex_selfee/Pokedex_App/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from Pokedex_selfee.Pokedex_App.views.types_pokemonviews import UserGroupAddView, UserGroupRemoveView
+from Pokedex_selfee.Pokedex_App.views.views import get_pokemon_view
 
 
 urlpatterns = [
+    path('pokemon/', get_pokemon_view, name='get_pokemon'),
     path('group/<str:pokemon_type>/add/', UserGroupAddView.as_view(), name='add_pokemon_type'),
     path('group/<str:pokemon_type>/remove/', UserGroupRemoveView.as_view(), name='remove_pokemon_type'),
 ]
